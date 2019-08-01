842955b065def2b4ce13a3355c54d560e58e5aa0
alexxxstep <astep2004@gmail.com>
2019-08-01 09:20:43 +0300

    correct

--- practice_04/pagination_1.py
+++ practice_04/pagination_1.py
@@ -2,7 +2,9 @@ import re
 
 
 class Page:
-    pass
+    def __init__(self, num, content):
+        self.num = num
+        self.content = content
 
 
 class Book:
@@ -10,6 +12,14 @@ class Book:
         self.pages = []
         self.bookmarks = {}
         self._current_page = None
+
+    @property
+    def current_page(self):
+        return self._current_page
+
+    @current_page.setter
+    def current_page(self, page):
+        self._current_page = page
 
     def add_page(self, page):
         self.pages.append(page)
@@ -33,20 +43,22 @@ class Book:
             left -= count
 
 
-def main():
-    txt = []
-    with open('advs1.txt', 'r') as f:
-        txt = f.readlines()
+# def main():
+txt = []
+with open('advs1.txt', 'r') as f:
+    txt = f.readlines()
 
-    if txt:
-        book = Book()
-        sentences = book.get_sentences(''.join(txt))
+if txt:
+    book = Book()
+    sentences = book.get_sentences(''.join(txt))
 
-        for p in book.chunks_sentences(sentences, 3):
-            page = ' '.join(p)
-            print(page)
-            book.add_page(page)
+    num = 1
+    for p in book.chunks_sentences(sentences, 3):
+        page_content = ' '.join(p)
+        page = Page(num, page_content)
+        # print(page)
+        book.add_page(page)
+    book.current_page = book.pages[0]
 
-
-if __name__ == '__main__':
-    main()
+# if __name__ == '__main__':
+#     main()
