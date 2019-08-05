244fce4c4e232cd85ecac2e59a127540a52363fa
alexxxstep <astep2004@gmail.com>
2019-08-05 17:10:19 +0300

    update game15

--- practice_05/game15.py
+++ practice_05/game15.py
@@ -1,4 +1,6 @@
 
+from  random import shuffle
+
 n = 1
 n_list = []
 
@@ -25,9 +27,13 @@ class Number:
 
 
 class Game:
+    
+    MOVES = ['a', 'd', 'w', 's']
+    
     def __init__(self):
         self.goal_variant = []
         self.current_variant = []
+        
 
     def is_game_finished(self):
         return self.goal_variant == self.current_variant
@@ -42,40 +48,74 @@ class Game:
                 n_iner.append(number_f)
                 n += 1
             self.goal_variant.append(n_iner)
+            
+    def create_current_variant(self):
+        random_moves = Game.MOVES * 2
+        shuffle(random_moves)
+        
+        self.current_variant = self.goal_variant.copy()
+        
+        for i in random_moves:
+            self.move(i) 
+        
+    
+    
 
     def result_print(self):
-        for x in self.goal_variant:
+        for x in self.current_variant:
             print('-' * 21)
             print(f'| {x[0].name} | {x[1].name} | {x[2].name} | {x[3].name} |')
-            if x == self.goal_variant[3]:
+            if x == self.current_variant[3]:
                 print('-' * 21)
 
     def get_index_16(self):
-        num = 16
-        for x in self.goal_variant:
-            for y in self.goal_variant.x:
-                pass
+        numb16 = 16
+        for x in self.current_variant:
+            for y in x:
+                if y._numb == numb16:
+                    return (self.current_variant.index(x), x.index(y))
+        else:
+            return (None,None)
 
-
-
-
-    def move_right(x, y):
-        pass
-
-    def move_left(x, y):
-        pass
-
-    def move_up(x, y):
-        pass
-
-    def move_down(x, y):
-        pass
-
+    
+    def move(self, key, comm = False):        
+        x,y = self.get_index_16()
+        comment_text = 'uncorrect move'
+        if key == 'a':
+            try:
+                self.current_variant[x][y],self.current_variant[x][y-1] = self.current_variant[x][y-1], self.current_variant[x][y]
+            except:
+                if comm:
+                    print(comment_text)
+        elif key == 'd':
+            try:
+                self.current_variant[x][y],self.current_variant[x][y+1] = self.current_variant[x][y+1], self.current_variant[x][y]
+            except IndexError:
+                if comm:
+                    print(comment_text)
+                
+        elif key == 'w':
+            try:
+                self.current_variant[x][y],self.current_variant[x-1][y] = self.current_variant[x-1][y], self.current_variant[x][y]
+            except:
+                if comm:
+                    print(comment_text)
+        elif key == 's':
+            try:
+                self.current_variant[x][y],self.current_variant[x+1][y] = self.current_variant[x+1][y], self.current_variant[x][y]
+            except:
+                if comm:
+                    print(comment_text)
+        else:
+            if comm:
+                    print(comment_text)
+            
+            
+   
 
 game = Game()
 
 game.create_goal_variant()
+game.create_current_variant()
 game.result_print()
-
-game.move_left()
-
+ 
